31630e11d30327af194adba63aa23a17532dc1a3
LLopezPabon <127800002+LLopezPabon@users.noreply.github.com>
2023-05-15 20:10:20 -0500

    Update join.js

--- join.js
+++ join.js
@@ -1,1 +1,12 @@
+function join(arreglo) {
+    let cadena = "";
 
+    arreglo.forEach((elemento) => {
+        cadena += elemento + " "
+         })
+        return cadena.trimEnd()
+    }
+
+console.log(join(["aloja" , "planeta"])); //"aloja,planeta"
+console.log(join(["make","it","real"])); // "make","it","real"
+console.log(join([])); // ""
